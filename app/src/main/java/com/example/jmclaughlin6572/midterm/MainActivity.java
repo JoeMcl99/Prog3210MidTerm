4d9a30312cc3d76f9e71fcc40bb2feaa30c572a6
jmclaughlin6572 <jmclaughlin6572@gmail.com>
2017-12-21 17:04:27 -0500

    added stuff

--- app/src/main/java/com/example/jmclaughlin6572/midterm/MainActivity.java
+++ app/src/main/java/com/example/jmclaughlin6572/midterm/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoSubActivicty(View view) {
+        Toast.makeText(getApplicationContext(), "Jordan McLaughlin", Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, subActivicty.class);
         startActivity(intent);
     }
@@ -21,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
     public void implicitIntent(View view) {
         Intent intent = new Intent();
         intent.setAction(intent.ACTION_SEND);
-        intent.putExtra(Intent.EXTRA_TEXT, "Second intent message lol");
         intent.setType("text/plain");
+
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
